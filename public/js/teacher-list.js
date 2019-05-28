74f317e8a381d0f815a96b85459846bf2b23c361
W <2427372199@qq.com>
2019-05-29 07:59:43 +0800

    完成讲师启用和注销功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -24,11 +24,40 @@ define(['jquery', 'template', 'bootstrap'], function ($, template) {
                     dataType: 'json',
                     success: function (data) {
                         //解析数据渲染页面
-                        var html = template('modalTpl',data.result);
-                        //console.log(data);
+                        var html = template('modalTpl', data.result);
+                        console.log(data);
                         $('#modalInfo').html(html);
                         //显示弹窗
                         $('#teacherModal').modal();
+                    }
+                });
+            });
+
+            //控制启用和注销
+            $('.eod').click(function () {
+                var td = $(this).closest('td');
+                var tcId = td.attr('data-tcId');
+                var tcStatus = td.attr('data-status');
+                var that = this;
+                $.ajax({
+                    type: 'post',
+                    url: '/api/teacher/handle',
+                    data: {
+                        tc_id: tcId,
+                        tc_status: tcStatus
+                    },
+                    dataType: 'json',
+                    success: function (data) {
+                        if (data.code == 200) {
+                            //修改当前状态
+                            td.attr('data-status', data.result.tc_status);
+                            //修改文字信息
+                            if (data.result.tc_status == 0) {
+                                $(that).html('注销');
+                            } else {
+                                $(that).html('启用');
+                            }
+                        }
                     }
                 });
             });
