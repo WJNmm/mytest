0fc2a43573825d809658453de3a2e9fb52dc3308
W <2427372199@qq.com>
2019-05-29 17:43:00 +0800

    添加日期选择插件

--- public/js/config.js
+++ public/js/config.js
@@ -5,16 +5,21 @@ require.config({
         cookie: 'jquery-cookie/jquery.cookie',
         template: 'artTemplate/template-web',
         bootstrap: 'bootstrap/js/bootstrap.min',
+        datepicker: 'bootstrap-datepicker/js/bootstrap-datepicker',
+        language: 'bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min',
         common: '../js/common',
         login: '../js/login',
         index: '../js/index',
         teacherlist: '../js/teacher-list',
         teacheradd: '../js/teacher-add',
-        util:'../js/util'
+        util: '../js/util'
     },
     shim: {
         bootstrap: {
             deps: ['jquery']
+        },
+        language: {
+            deps: ['jquery', 'datepicker']
         }
     }
 })
