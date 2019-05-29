fa4a04542a3c4d7ee189df024540787bcf819275
W <2427372199@qq.com>
2019-05-29 16:53:23 +0800

    完成讲师添加和编辑功能

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -19,12 +19,33 @@ define(['jquery', 'template', 'util'], function ($, template, util) {
                 var html = template('teacherTpl', data.result);
                 //console.log(data);
                 $('#teacherInfo').html(html);
+                //提交编辑讲师表单
+                submitForm('/api/teacher/update');
             }
         });
     } else {
         //添加操作
         var html = template('teacherTpl', {operate: '添加讲师', tc_gender: 1});
         $('#teacherInfo').html(html);
+        //提交添加讲师表单
+        submitForm('/api/teacher/add');
+    }
+    //提交表单公用方法
+    function submitForm(url) {
+        $('#teacherBtn').click(function () {
+            $.ajax({
+                type: 'post',
+                url: url,
+                data: $('#teacherForm').serialize(),
+                dataType: 'json',
+                success: function (data) {
+                    if (data.code == 200) {
 
+                        location.href = '/teacher/list';
+                        console.log(data);
+                    }
+                }
+            });
+        });
     }
 });
