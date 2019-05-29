055c08d35c583f0a026a9f4f0adb58df13fcd77b
W <2427372199@qq.com>
2019-05-29 08:47:59 +0800

    左侧导航菜单设置

---
+++ public/js/util.js
@@ -0,0 +1,9 @@
+define(['jquery'], function ($) {
+    //工具函数
+    return {
+        setMenu: function (path) {
+            $('.navs a[href="' + path + '"]').addClass('active');
+        }
+    }
+
+});
