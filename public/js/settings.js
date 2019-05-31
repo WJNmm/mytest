241fdc4412e6da81b77de57cf16784635bdfab2b
W <2427372199@qq.com>
2019-05-31 08:37:17 +0800

    完成个人中心表单提交

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,4 +1,4 @@
-define(['jquery', 'template', 'util', 'uploadify','datepicker','language','region'], function ($, template, util) {
+define(['jquery', 'template', 'util', 'ckeditor', 'uploadify', 'datepicker', 'language', 'region', 'validate', 'form'], function ($, template, util, CKIDITOR) {
     //设置导航菜单选中
     util.setMenu('/main/index');
     //调用后台接口填充表单
@@ -28,8 +28,46 @@ define(['jquery', 'template', 'util', 'uploadify','datepicker','language','regio
 
             //处理省市县三级联动
             $('#pcd').region({
-                url:'/public/assets/jquery-region/region.json'
-            })
+                url: '/public/assets/jquery-region/region.json'
+            });
+
+            //处理富文本
+            CKIDITOR.replace('ckeditor', {
+                toolbarGroups: [
+                    {name: 'clipboard', groups: ['clipboard', 'undo']},
+                    {name: 'editing', groups: ['find', 'selection', 'spellchecker', 'editing']},
+                    {name: 'links', groups: ['links']}
+                ]
+            });
+
+            // 处理表单提交
+            $('#settingsForm').validate({
+                sendForm : false,
+                valid : function(){
+                    // 把富文本的数据同步到表单域中
+                    for(var instance in CKEDITOR.instances){
+                        CKEDITOR.instances[instance].updateElement();
+                    }
+                    // 获取家乡数据
+                    var p = $('#p options:selected').text();
+                    var c = $('#c options:selected').text();
+                    var d = $('#d options:selected').text();
+                    var hometown = p + '|' + c + '|' + d;
+                    // 所有验证都通过，提交表单
+                    $(this).ajaxSubmit({
+                        type : 'post',
+                        url : '/api/teacher/modify',
+                        data : {tc_hometown:hometown},
+                        dataType : 'json',
+                        success : function(data){
+                            if(data.code == 200){
+                                // 刷新页面
+                                location.reload();
+                            }
+                        }
+                    });
+                }
+            });
 
         }
     });
