7a69a3bc59b96a671923f2e02c813edcc179127e
W <2427372199@qq.com>
2019-06-01 21:03:09 +0800

    编辑和添加课时共用模态框

--- public/js/course-lesson.js
+++ public/js/course-lesson.js
@@ -1,4 +1,4 @@
-define(['jquery', 'template', 'util'], function ($, template, util) {
+define(['jquery', 'template', 'util', 'bootstrap', 'form'], function ($, template, util) {
     // 设置导航菜单选中
     util.setMenu('/course/list');
     // 获取课程ID
@@ -14,6 +14,33 @@ define(['jquery', 'template', 'util'], function ($, template, util) {
             var html = template('lessonTpl', data.result);
             $('#lessonInfo').html(html);
 
+            // 处理添加功能
+            $('#addBtn').click(function () {
+                var html = template('modalTpl', {operate: '添加课时'});
+                $('#modalInfo').html(html);
+                $('#chapterModal').modal();
+                // 添加提交表单
+                //submitForm('/api/course/chapter/add');
+            });
+            // 处理编辑功能
+            $('.editLesson').click(function () {
+                // 先查询数据
+                var ctId = $(this).attr('data-ctId');
+                $.ajax({
+                    type: 'get',
+                    url: '/api/course/chapter/edit',
+                    data: {ct_id: ctId},
+                    dataType: 'json',
+                    success: function (data) {
+                        // 渲染模板
+                        $('#modalInfo').html(html);
+                        // 显示弹窗
+                        $('#chapterModal').modal();
+                        // 编辑课时提交表单
+                        //submitForm('/api/course/chapter/modify', ctId);
+                    }
+                });
+            });
         }
     });
 });
